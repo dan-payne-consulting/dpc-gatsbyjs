77f3f81de085f4ca58f5bfa95e6db260ee7dde8c
Dan Payne <dan@tranquilitydata.com>
2019-04-07 15:23:01 -0700

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,9 +7,10 @@ const About = () => (
     <Layout>
         <SEO title="About Dan Payne" />
         <h1>About Dan Payne</h1>
+        <p>After ten years on active duty in the U.S. Air Force as a satellite and microwave technician, I found myself working closely with a number of engineering officers. I was insipired to become an engineer myself but knew it would take forever to finish my schooling with the pace of deployments. I decided to transfer to the California Air National Guard to continue my service while pursuing my degree full-time.</p>
         <p>In 2000, I completed my Electronics Engineering Technology degree at DeVry University and after working for Boeing Satellite Systems and for REMEC/Cobham designing flight/space hardware, I became restless as I realized I preferred working with code.</p>
         <p>I began self-schooling on web development concepts and in 2007 I began working with Spiegel Design Group (SDG) as the sole backend developer. Over the years we grew the company, working with clients such as The Drybar, IT Cosmetics, ReserveBar, simplehuman, Korn Ferry, and many others. As the team grew, my role became more managerial as I ran the day-to-day operations of the company, but I continued to contribute as a developer as needed.</p>
-        <p>After leaving SDG I realized that my years of experience as a developer would give me a solid foundation in the rapidly emerging web accessibility field. I trained for and passed the IAAP WAS certification exam recently and now continue to develop and grow my skills in evaluating website accessibility.</p>
+        <p>When I left SDG I realized that my years of experience as a developer would give me a solid foundation in the rapidly emerging web accessibility field. I trained for and passed the IAAP WAS certification exam recently and now continue to develop and grow my skills in evaluating website accessibility.</p>
     </Layout>
 )
 

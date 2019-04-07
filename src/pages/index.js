8c70da52a1b5b8b2280ad6b0b32b115f90a1bd62
Dan Payne <dan@tranquilitydata.com>
2019-04-07 14:42:20 -0700

    Add comma on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = () => (
 
         <p>According to <a target="_blank" rel="noopener noreferrer" href="https://blog.usablenet.com/a-recap-of-2018-ada-web-accessibility-lawsuits-and-primer-on-how-to-avoid-them?utm_campaign=AQA%20Free%20Test&utm_content=87096829&utm_medium=social&utm_source=facebook&hss_channel=fbp-111272465575315">Usablenet.com</a>, web accessibility lawsuits in the United States jumped by 181% in 2018 from the previous year. We've all heard the stories of more and more high-profile businesses being sued by users who are unable to use their websites. In some cases these lawsuits originated from actual, everyday users who simply couldn't accomplish something, but many are thought to have been originated by predatory lawyers seeing an easy opportunity to make money by exploiting the fact that so many sites have these issues.</p>
 
-        <p>And yet, there is no magic pill that will fix all errors. Even with the best advice and technology it's probable that not all errors can (or should) be fixed. That shouldn't stop us from still doing whatever we can to make our sites as widely available to as many users as we can. It also means that the decision on what to fix and how to fix it is a collaborative effort that can only start with a thorough site evaluation.</p>
+        <p>And yet, there is no magic pill that will fix all errors. Even with the best advice and technology, it's probable that not all errors can (or should) be fixed. That shouldn't stop us from still doing whatever we can to make our sites as widely available to as many users as we can. It also means that the decision on what to fix and how to fix it is a collaborative effort that can only start with a thorough site evaluation.</p>
       </article>
     </section>
 

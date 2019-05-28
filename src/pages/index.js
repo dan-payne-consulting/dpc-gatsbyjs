0de6170fd97418e6297c587c428b7e6b33ac3075
Dan Payne <dan@tranquilitydata.com>
2019-05-28 16:00:59 -0700

    Update home page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,27 +10,30 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`accessibility`, `consulting`, `website`]} />
 
-    <section>
+    <section
+    /*style={{
+    display: `grid`,
+    gridTemplateColumns: `1fr 1fr`,
+    gridTemplateRows: `1fr`,
+    gridColumnGap: `10px`,
+    gridRowGap: `0px`
+    }}*/
+    >
       <article className="content-text">
-<div>
-        <h1>Web Accessibility Specialist and Consultant</h1>
+          <div>
+              <h1>Web Accessibility Specialist and Consultant</h1>
 
-        <p style={{textAlign: `center`, paddingTop: `20px`}}>
-          <a href="https://www.accessibilityassociation.org/certification">
-            <img src={cpwaLogo} alt="About IAAP Certification"/>
-          </a>
-        </p>
+              <p style={{textAlign: `center`, paddingTop: `20px`}}>
+                  <a href="https://www.accessibilityassociation.org/certification">
+                      <img src={cpwaLogo} alt="About I A A P Certification"/>
+                  </a>
+              </p>
 
-        <p><img class="inline-img" src={headshot} alt="" style={{float:`left`,borderRadius:`45px`,margin:`0 30px 10px 0`,boxShadow:`20px 20px 15px -25px #000`,maxWidth:`33%`}}/>With over 12 years of experience as a web developer, as a member of the <a target="_blank" rel="noopener noreferrer" href="https://www.accessibilityassociation.org">International Association of Accessibility Professionals (IAAP)</a>, and as an <a target="_blank" rel="noopener noreferrer" href="https://www.accessibilityassociation.org/certification">IAAP Certified Professional in Web Accessibility</a> I can help evaluate your website and identify, prioritize, and recommend remediation for accessibility issues.</p>
-
-        <p>According to <a target="_blank" rel="noopener noreferrer" href="https://webaim.org/">WebAIM.org</a>, as of February 2019, 97.8% of <a target="_blank" rel="noopener noreferrer" href="https://webaim.org/projects/million/">the top million website home pages</a> have automatically detectable accessibility errors, making them difficult or impossible for some users to interact with. They go on to say that "only around 25% of possible conformance failures can be automatically detectable." That means you need an experienced web accessibility professional to detect the nearly 75% of issues that automated tools can't catch.</p>
-
-        <p>According to <a target="_blank" rel="noopener noreferrer" href="https://blog.usablenet.com/a-recap-of-2018-ada-web-accessibility-lawsuits-and-primer-on-how-to-avoid-them?utm_campaign=AQA%20Free%20Test&utm_content=87096829&utm_medium=social&utm_source=facebook&hss_channel=fbp-111272465575315">Usablenet.com</a>, web accessibility lawsuits in the United States jumped by 181% in 2018 from the previous year. We've all heard the stories of more and more high-profile businesses being sued by users who are unable to use their websites. In some cases these lawsuits originated from actual, everyday users who simply couldn't accomplish something, but many are thought to have been originated by predatory lawyers seeing an easy opportunity to make money by exploiting the fact that so many sites have these issues.</p>
-
-        <p>And yet, there is no magic pill that will fix all errors. Even with the best advice and technology, it's probable that not all errors can (or should) be fixed. That shouldn't stop us from still doing whatever we can to make our sites as widely available to as many users as we can. It also means that the decision on what to fix and how to fix it is a collaborative effort that can only start with a thorough site evaluation.</p>
-</div>
+              <p><img class="inline-img" src={headshot} alt="" style={{float:`left`,borderRadius:`45px`,margin:`0 30px 10px 0`,boxShadow:`20px 20px 15px -25px #000`,maxWidth:`33%`}}/>After 12 plus years in web development, I've dedicated myself to help make the web a more inclusive and accessible place for everyone. I've recently completed the training and certification required to become a <a target="_blank" rel="noopener noreferrer" href="https://www.accessibilityassociation.org/certification">Certified Professional in Web Accessibility</a> with the <a target="_blank" rel="noopener noreferrer" href="https://www.accessibilityassociation.org">International Association of Accessibility Professionals (IAAP)</a>, a not-for-profit organization whose aim is "to help accessibility professionals develop and advance their careers and to help organizations integrate accessibility into their products and infrastructure."</p>
+              <p>I'm passionate about helping people, so it's a natural fit for me to help increase awareness of the issues people with disabilities have when interacting with the web. For too long, the development community has, sometimes accidentally and sometimes intentionally, excluded whole sectors of the population from being able to consume, interact with and contribute to the web. Unfortunately the latest trends are showing that the situation is getting worse and not better. I believe it's up to veterans like me to spread the word about some simple methods young developers can use to make their sites more accessible and maybe along the way change the minds of some other veterans who don't believe it's worth the time or effort.</p>
+          </div>
       </article>
-    </section>
+  </section>
 
   </Layout>
 )

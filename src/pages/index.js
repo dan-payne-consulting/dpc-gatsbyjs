02836bf977d427b4756240751285e58679b525da
Dan Payne <dan@tranquilitydata.com>
2019-10-31 14:27:08 -0700

    Revert "Update wording on home page"
    This reverts commit 4bd9facadd5e249f65f2e7de2adbf39a23e5138c.

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => (
     >
       <article className="content-text">
           <div>
-              <h1>Web Accessibility Consulting</h1>
+              <h1>Web Accessibility Specialist and Consultant</h1>
 
               <p style={{textAlign: `center`, paddingTop: `20px`}}>
                   <a href="https://www.accessibilityassociation.org/certification">
@@ -31,7 +31,7 @@ const IndexPage = () => (
 
               <p><img className="inline-img" src={headshot} alt="" style={{float:`left`,borderRadius:`45px`,margin:`0 30px 10px 0`,boxShadow:`20px 20px 15px -25px #000`,maxWidth:`33%`}}/>I'm a web technologist. My solid background in electronics engineering and backend web development as well as several leadership and management roles over the years give me a strong understanding of how businesses work and what they need for success on the web.</p>
               <p>I'm also a dedicated advocate for making the web a more inclusive and accessible place for everyone. I'm a <a target="_blank" rel="noopener noreferrer" href="https://www.accessibilityassociation.org/certification">Certified Professional in Web Accessibility</a> through the <a target="_blank" rel="noopener noreferrer" href="https://www.accessibilityassociation.org">International Association of Accessibility Professionals (IAAP)</a>, a not-for-profit organization whose aim is "to help accessibility professionals develop and advance their careers and to help organizations integrate accessibility into their products and infrastructure."</p>
-              <p>Bringing these two things together: a wealth of experience and a strong desire to help people, my consultancy is all about making the web an equitable place for everyone AND providing the strategies, vision, and expertise my clients' need to successfully post their brand to the world.</p>
+              <p>Bringing these two things together: a wealth of experience and a strong desire to help people, my consultancy is all about making the web a better place for everyone AND providing the strategies, vision, and expertise my clients' need to successfully post their brand to the world.</p>
               <p>If you're looking for expert advice on web design, development, accessibility, auditing, compliance, remediation, or anything else web-related, please <a href="/contact">get in touch</a> and I'd be happy to help.</p>
           </div>
       </article>
